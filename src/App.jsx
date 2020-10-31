c676b4d7a38cae7d4235f304089459e771a72e5d
nikhilnair23 <nikhilnair238@gmail.com>
2020-10-31 19:45:28 -0400

    Created input fields and displayed messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+import Chat from './Chat'
+
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <Chat/>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
