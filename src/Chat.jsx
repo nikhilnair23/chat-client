6610da45f6d28c6bd1f5fd1d6a13fc95f060d09b
nikhilnair23 <nikhilnair238@gmail.com>
2020-10-31 20:26:33 -0400

    Added websockets

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, useQuery, useSubscription, gql, useMutation} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
 import {
     Container,
@@ -9,13 +10,24 @@ import {
     Button
 } from 'shards-react'
 
+
+const link = new WebSocketLink({
+    uri: `ws://localhost:4000/`,
+    options: {
+        reconnect: true
+    }
+});
+
 const client = new ApolloClient({
+    link,
     uri: 'http://localhost:4000/',
     cache: new InMemoryCache()
 });
 
+
+
 const GET_MESSAGES = gql`
-    query{
+    subscription{
         messages{
             id
             content
@@ -32,9 +44,7 @@ mutation ($user:String!, $content:String!){
 
 
 const Messages = ({user}) => {
-    const {data} = useQuery(GET_MESSAGES, {
-        pollInterval: 500
-    });
+    const {data} = useSubscription(GET_MESSAGES);
     if (!data) {
         return null;
     }
